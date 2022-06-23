6c7dc45073a44e168c05ab519fdef15edf8740b4
isfacademy <isfacademy.2023@gmail.com>
2022-06-23 15:12:17 +0300

    separete login form

---
+++ lib/screens/login/components/login_form.dart
@@ -0,0 +1,77 @@
+import 'package:ecommerce_admin_panel/controllers/MenuController.dart';
+import 'package:ecommerce_admin_panel/controllers/auth_controller.dart';
+import 'package:ecommerce_admin_panel/shared/components/default_button.dart';
+import 'package:ecommerce_admin_panel/shared/components/default_text_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+
+class LoginForm extends StatelessWidget {
+  LoginForm({Key? key}) : super(key: key);
+  var text_emailcontroller = TextEditingController();
+  var text_passwordcontroller = TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formkey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Login Screen",
+            style: TextStyle(fontSize: 30),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          defaultTextFormField(
+            controller: text_emailcontroller,
+            inputtype: TextInputType.emailAddress,
+            hinttext: "Email",
+            onvalidate: (value) {
+              if (value!.isEmpty) return "email must not be empty";
+            },
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          defaultTextFormField(
+            controller: text_passwordcontroller,
+            inputtype: TextInputType.visiblePassword,
+            hinttext: "Password",
+            onvalidate: (value) {
+              if (value!.isEmpty) return "password must not be empty";
+            },
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          context.watch<AuthController>().isloadingSignIn
+              ? CircularProgressIndicator()
+              : defaultButton(
+                  text: "Sign In",
+                  height: 50,
+                  onpress: () {
+                    if (_formkey.currentState!.validate()) {
+                      context.read<AuthController>()
+                        ..signIn(text_emailcontroller.text.trim(),
+                                text_passwordcontroller.text.toString())
+                            .then((value) {
+                          if (value != null) {
+                            text_emailcontroller.clear();
+                            text_passwordcontroller.clear();
+                            print("sign in successfully");
+                            context.read<MenuController>()..buildMenu();
+                          } else {}
+                        });
+                    }
+                  }),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_admin_panel/controllers/MenuController.dart';
 import 'package:ecommerce_admin_panel/controllers/auth_controller.dart';
+import 'package:ecommerce_admin_panel/screens/login/components/login_form.dart';
 import 'package:ecommerce_admin_panel/shared/components/default_button.dart';
 import 'package:ecommerce_admin_panel/shared/components/default_text_form.dart';
 import 'package:ecommerce_admin_panel/shared/constants.dart';
@@ -7,15 +8,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:provider/provider.dart';
-import 'package:top_snackbar_flutter/custom_snack_bar.dart';
-import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
-  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-
-  var text_emailcontroller = TextEditingController();
-  var text_passwordcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -36,64 +31,7 @@ class LoginScreen extends StatelessWidget {
         padding: EdgeInsets.all(defaultPadding),
         child: Padding(
           padding: const EdgeInsets.all(30.0),
-          child: Form(
-            key: _formkey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Login Screen",
-                  style: TextStyle(fontSize: 30),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                defaultTextFormField(
-                  controller: text_emailcontroller,
-                  inputtype: TextInputType.emailAddress,
-                  hinttext: "Email",
-                  onvalidate: (value) {
-                    if (value!.isEmpty) return "email must not be empty";
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                defaultTextFormField(
-                  controller: text_passwordcontroller,
-                  inputtype: TextInputType.visiblePassword,
-                  hinttext: "Password",
-                  onvalidate: (value) {
-                    if (value!.isEmpty) return "password must not be empty";
-                  },
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                context.watch<AuthController>().isloadingSignIn
-                    ? CircularProgressIndicator()
-                    : defaultButton(
-                        text: "Sign In",
-                        height: 50,
-                        onpress: () {
-                          if (_formkey.currentState!.validate()) {
-                            context.read<AuthController>()
-                              ..signIn(text_emailcontroller.text.trim(),
-                                      text_passwordcontroller.text.toString())
-                                  .then((value) {
-                                if (value != null) {
-                                  text_emailcontroller.clear();
-                                  text_passwordcontroller.clear();
-                                  print("sign in successfully");
-                                  context.read<MenuController>()..buildMenu();
-                                } else {}
-                              });
-                          }
-                        }),
-              ],
-            ),
-          ),
+          child: LoginForm(),
         ));
   }
 }
